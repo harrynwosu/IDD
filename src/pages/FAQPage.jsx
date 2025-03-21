10c0d5488c8f60b56cdf2f96812d0afe3402aa44
Hector <lhectorm22@gmail.com>
2025-03-21 00:56:41 -0500

    commit updating text to FAQ page

--- src/pages/FAQPage.jsx
+++ src/pages/FAQPage.jsx
@@ -7,16 +7,36 @@ const FAQPage = () => {
 
     const faqData = [
         {
-            question: "How do I reset my password?",
-            answer: "Click on the ‘Forgot Password’ link on the login page and follow the instructions.",
+            question: "How do I search for service providers",
+            answer: "You can use the search bar to find providers by name, location, or service type. A map view is also available.",
         },
         {
-            question: "How long does shipping take?",
-            answer: "Standard shipping takes 3-5 business days. Express shipping takes 1-2 business days.",
+            question: "Where does the provider information come from?",
+            answer: "Our data is collected from public sources and state agencies.",
         },
         {
-            question: "What payment methods do you accept?",
-            answer: "We accept all major credit cards, PayPal, and Apple Pay.",
+            question: "What if I find incorrect or outdated information?",
+            answer: "You can report incorrect details using the Thumbs up/down feature on provider pages.",
+        },
+        {
+            question: "Are all service providers licensed or verified?",
+            answer: "We aim to include only legitimate providers, but we encourage users to verify credentials before seeking services.",
+        },
+        {
+            question: "Can I filter providers by service type?",
+            answer: "Yes! You can filter results by categories such as resdential care, employment support, therapy services, etc.",
+        },
+        {
+            question: "How do I contact a service provider?",
+            answer: "Each provider listing includes contact details such as phone number and address.",
+        },
+        {
+            question: "I'm having trouble using the website. Who do I contact?",
+            answer: "You can reach out via our Contact Us page for technical support.",
+        },
+        {
+            question: "Is my personal data safe on this website?",
+            answer: "We prioritize user privacy and do not collect personal information.",
         },
     ];
 
