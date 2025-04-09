a3c28d22c69e5a27f32790a29a69ff6e9f9de556
Harold Nwosu <harold.nwosu@gmail.com>
2025-04-09 07:29:28 -0500

    typo fix

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,11 +1,11 @@
-import { useNavigate } from 'react-router-dom' ;
+import { useNavigate } from 'react-router-dom';
 import HeroImage from '/idd-homepage.png';
 import AboutImage from '/about-us-idd.png';
 
-import '../styles/Homepage.css';
+import '../styles/HomePage.css';
 
 const HomePage = () => {
-    const navigate = useNavigate(); 
+    const navigate = useNavigate();
     return (
         <div className='landing-page'>
             <section className='home-hero'>
@@ -19,14 +19,22 @@ const HomePage = () => {
                 <div className='content-container'>
                     <h2 className='content-title'>Keystone Alliance</h2>
                     <p className='content-text'>
-                        Accessing essential services for individuals with Intellectual and Developmental Disabilities (IDD) 
-                    can be a complex and time-consuming process. This website is designed to streamline this experience by 
-                    providing a centralized platform to locate and connect with verified support services, 
-                    including healthcare, therapy, education, employment assistance, and community programs.
+                        Accessing essential services for individuals with
+                        Intellectual and Developmental Disabilities (IDD) can be
+                        a complex and time-consuming process. This website is
+                        designed to streamline this experience by providing a
+                        centralized platform to locate and connect with verified
+                        support services, including healthcare, therapy,
+                        education, employment assistance, and community
+                        programs.
                     </p>
                     {/* Updated Button */}
-                    <button className='learn-more-button'
-                    onClick= {() => navigate('/services')}>Search For Services</button>
+                    <button
+                        className='learn-more-button'
+                        onClick={() => navigate('/services')}
+                    >
+                        Search For Services
+                    </button>
                 </div>
             </section>
 
@@ -39,13 +47,25 @@ const HomePage = () => {
                         <div className='about-text'>
                             <h3 className='about-heading'>Our Mission</h3>
                             <p className='about-description'>
-                                Our goal is to enhace accessibility, promote independence, and improve
-                                the quality of life for individuals with IDD and their families.
+                                Our goal is to enhace accessibility, promote
+                                independence, and improve the quality of life
+                                for individuals with IDD and their families.
                             </p>
                             <p className='about-description'>
-                                The Keystone Alliance team has decades of nonprofit experience and a demonstrated track record of strengthening the financial and mission performance of its client organizations. 
-                            From regular weekly check-ins with your team to share updates and report on results, to an embedded Keystone liaison to work on-site at your organization so your team has an expert-on-hand to support your operations and serve as a sounding board for new ideas.  
-                            We work together with our customer organization to create and deliver innovative, long-term solutions that make a real and lasting mission impact.
+                                The Keystone Alliance team has decades of
+                                nonprofit experience and a demonstrated track
+                                record of strengthening the financial and
+                                mission performance of its client organizations.
+                                From regular weekly check-ins with your team to
+                                share updates and report on results, to an
+                                embedded Keystone liaison to work on-site at
+                                your organization so your team has an
+                                expert-on-hand to support your operations and
+                                serve as a sounding board for new ideas. We work
+                                together with our customer organization to
+                                create and deliver innovative, long-term
+                                solutions that make a real and lasting mission
+                                impact.
                             </p>
                         </div>
                         <div className='about-image'>
